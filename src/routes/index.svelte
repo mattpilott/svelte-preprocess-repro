639da28a131ca7a42839c637d2b6a786cbd9e7e0
Matt Pilott <matt@creativelittledots.co.uk>
2020-04-12 12:48:16 +0100

    Using Sass

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<style lang="scss">
+	:global(.block) {
+      color: red;
+
+      &__element {
+         color: purple;
+
+         &--modifier {
+            color: blue;
+         }
+      }
+   }
+</style>
+
+<div class="block">
+   Block <span class="block__element">Element</span> <span class="block__element--modifier">Modifier</span>
+</div>
